130c010c822f760fe636b0df081c49dbdc4b0ff4
Paolo Flores <pflores.fisi22@gmail.com>
2024-04-16 21:06:41 -0500

    add interfaces

--- core/IList_circular.js
+++ core/IList_circular.js
@@ -1,3 +1,31 @@
-class IList {
+import { DoubleNode } from './double_node';
 
+class IListCircular {
+  addFirst(newElem) {}
+
+  addLast(newElem) {}
+
+  insertAt(index, newElem) {}
+
+  isEmpty() {}
+
+  contains(elem) {}
+
+  getSize() {}
+
+  getIndexOf(elem) {}
+
+  getFirst() {}
+
+  getLast() {}
+
+  getAt(index) {}
+
+  toString() {}
+
+  removeFirst() {}
+
+  removeLast() {}
+
+  removeAll() {}
 }
